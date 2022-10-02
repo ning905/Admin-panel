79448f9939d57b11f5ea6f3b7619c778b6b93d13
Ning Ma <ningma905@gmail.com>
2022-10-02 14:42:31 +0100

    chore: typo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/home/Home"
 import List from "./pages/list/List"
 import Login from "./pages/login/Login"
 import New from "./pages/new/New"
-import Single from "./pages/single/single"
+import Single from "./pages/single/Single"
 
 function App() {
 	return (
