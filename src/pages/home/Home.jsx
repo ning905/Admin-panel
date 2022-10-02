c2e94a2af57e8c561da0ce9cda6df53af0ad7e6f
Ning Ma <ningma905@gmail.com>
2022-10-02 14:42:49 +0100

    init: home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,15 @@
+import Navbar from "../../components/navbar/Navbar"
+import Sidebar from "../../components/sidebar/Sidebar"
 import "./home.scss"
 
 export default function Home() {
-	return <div className="home"></div>
+	return (
+		<div className="home">
+			<Sidebar />
+			<div className="home-container">
+				<Navbar />
+				home container
+			</div>
+		</div>
+	)
 }
