98cba1be7505a57fdabf30636a8c0d5eb9cb907c
Ning Ma <ningma905@gmail.com>
2022-10-07 19:11:52 +0100

    feat: single page

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,5 +1,16 @@
+import DataTable from "../../components/dataTable/DataTable"
+import Navbar from "../../components/navbar/Navbar"
+import Sidebar from "../../components/sidebar/Sidebar"
 import "./list.scss"
 
 export default function List() {
-	return <div>List</div>
+	return (
+		<div className="list">
+			<Sidebar />
+			<div className="list-container">
+				<Navbar />
+				<DataTable />
+			</div>
+		</div>
+	)
 }
--- src/pages/single/single.jsx
+++ src/pages/single/single.jsx
@@ -1,5 +1,56 @@
 import "./single.scss"
+import Navbar from "../../components/navbar/Navbar"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Chart from "../../components/chart/Chart"
+import RevenueTable from "../../components/revenueTable/RevenueTable"
 
 export default function Single() {
-	return <div>single</div>
+	return (
+		<div className="single">
+			<Sidebar />
+			<div className="single-container">
+				<Navbar />
+				<div className="top">
+					<div className="left">
+						<div className="edit-button">Edit</div>
+						<h1 className="title">Information</h1>
+						<div className="item">
+							<img
+								src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+								alt="item"
+								className="item-img"
+							/>
+							<ul className="details">
+								<h1 className="item-title">Jan Doe</h1>
+								<li className="detail-item">
+									<span className="item-key">Email:</span>
+									<span className="item-value">janedoe@gmail.com</span>
+								</li>
+								<li className="detail-item">
+									<span className="item-key">Phone:</span>
+									<span className="item-value">012345678</span>
+								</li>
+								<li className="detail-item">
+									<span className="item-key">Address:</span>
+									<span className="item-value">W Mosley St, Manchester</span>
+								</li>
+								<li className="detail-item">
+									<span className="item-key">Country:</span>
+									<span className="item-value">UK</span>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div className="right">
+						<Chart aspect={3 / 1} title="User Spending (Last 6 Months)" />
+					</div>
+				</div>
+				<div className="bottom">
+					<h1 className="title">Last Transactions</h1>
+
+					<RevenueTable />
+				</div>
+			</div>
+		</div>
+	)
 }
