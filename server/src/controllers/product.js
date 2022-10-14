d62d985126f344579258d3dc1f9bb61f229ebbba
Ning Ma <ningma905@gmail.com>
2022-10-14 17:35:02 +0100

    feat: admin or seller can delete product by id

--- server/src/controllers/product.js
+++ server/src/controllers/product.js
@@ -1,5 +1,5 @@
 import dbClient from "../utils/dbClient.js"
-import { InternalServerError } from "../utils/Error.js"
+import { InternalServerError, NoAccessError } from "../utils/Error.js"
 import {
 	sendDataResponse,
 	sendMessageResponse,
@@ -26,6 +26,29 @@ export async function getAllProductsForUser(req, res) {
 	try {
 		const products = await dbClient.product.findMany(query)
 		sendDataResponse(res, 200, products)
+	} catch (err) {
+		sendMessageResponse(res, serverError.code, serverError.message)
+		throw err
+	}
+}
+
+export async function deleteProductById(req, res) {
+	const id = req.params.id
+	const foundProduct = await dbClient.product.findUnique({ where: { id } })
+	if (!foundProduct) {
+		const notFound = new NotFoundError("product", "id")
+		return sendMessageResponse(res, notFound.code, notFound.message)
+	}
+	console.log("foundProduct: ", foundProduct)
+	console.log("req user: ", req.user.role)
+	if (req.user.id !== foundProduct.sellerId && req.user.role !== "ADMIN") {
+		const noAccess = new NoAccessError("Only the seller or admin can access")
+		return sendMessageResponse(res, noAccess.code, noAccess.message)
+	}
+
+	try {
+		const deletedProduct = await dbClient.product.delete({ where: { id } })
+		return sendDataResponse(res, 200, deletedProduct)
 	} catch (err) {
 		sendMessageResponse(res, serverError.code, serverError.message)
 		throw err
--- server/src/routes/product.js
+++ server/src/routes/product.js
@@ -1,9 +1,14 @@
 import { Router } from "express"
-import { getAllProductsForUser } from "../controllers/product.js"
+import {
+	deleteProductById,
+	getAllProductsForUser,
+} from "../controllers/product.js"
 import { validateAuthentication } from "../middleware/auth.js"
 
 const router = Router()
 
 router.get("/", validateAuthentication, getAllProductsForUser)
+
+router.delete("/:id", validateAuthentication, deleteProductById)
 
 export default router
